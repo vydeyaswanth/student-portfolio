af424f3447e9d72cf7a0fb1cc9be7f200505412b
Vikas-patidar <vikas.patidar@cogoport.com>
2023-10-22 15:20:20 +0530

    navigation add

--- student/ui/page-components/layout/navbar/index.js
+++ student/ui/page-components/layout/navbar/index.js
@@ -1,4 +1,13 @@
+import styles from './styles.module.css'
 const NavBar=()=>{
-    return <div>hello</div>
+    return <div className={styles.container}>
+        <div>ok</div>
+        <div className={styles.nev}>
+            <div>Home</div>
+            <div>About</div>
+            <div>Service</div>
+            <div>Team</div>
+        </div>
+    </div>
 };
 export default NavBar
